eebabb6103342ad29f5935e884e7bd691eaab9e4
Mathisfun123 <sairajareddykurelli@gmail.com>
2020-03-22 23:16:12 -0500

    cowjump.java: 2/12 test cases (got 4/12 in previous code) - need to prevent time out

--- src/cowjump.java
+++ src/cowjump.java
@@ -1,4 +1,5 @@
 import javax.sound.sampled.Line;
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -11,7 +12,7 @@ public class cowjump {
 		Scanner sc = new Scanner(new File("cowjump" + ".in"));
 		int n = sc.nextInt();
 		sc.nextLine();
-		LineSegment[] arr = new LineSegment[n];
+		Point[] arr = new Point[2*n];
 		int min = Integer.MAX_VALUE;
 		int max = Integer.MIN_VALUE;
 		for(int i = 0; i< n; i++){
@@ -22,81 +23,93 @@ public class cowjump {
 			if(ax< bx || (ax==bx && ay<by)) {
 				min = Math.min(ax,min);
 				max = Math.max(bx, max);
-				arr[i] = new LineSegment(ax, ay, bx, by);
+				arr[2*i] = new Point(ax,ay, i,true,null);
+				arr[2*i+1]= new Point(bx, by, i,false,arr[i]);
+				arr[2*i].pair= arr[2*i+1];
 			}else{
 				min = Math.min(bx,min);
 				max = Math.max(ax, max);
-				arr[i] = new LineSegment(bx, by, ax, ay);
+				arr[2*i] = new Point(bx, by, i, true,null);
+				arr[2*i+1] = new Point(ax, ay, i, false, arr[i]);
+				arr[2*i].pair= arr[2*i+1];
 			}
-
 		}
 		Arrays.sort(arr);
-		int[] intersections = new int[n];
-		int oneposs=-1,otherposs=-1;
-		System.out.println(Arrays.toString(arr));
-		List<LineSegment> currentlyAdded = new ArrayList<>();
-		HashMap<Integer, ArrayList<LineSegment>> endXvalforcurrent = new HashMap<>();
 		int lastposs = 0;
-		for(int i = min; i<=max; i ++){
-			if(endXvalforcurrent.containsKey(i)){
-				ArrayList<LineSegment> t = endXvalforcurrent.get(i);
-				for(LineSegment p: t){
-					currentlyAdded.remove(p);
+		HashSet<Point> currentSegments = new HashSet<>();
+		int[] intersections = new int[n];
+		Point onepoint = null; Point otherpoint = null;
+		for(int i = min;i<=max && lastposs!=2*n && (otherpoint==null|| (currentSegments.contains(onepoint)||currentSegments.contains(otherpoint)));i++){
+			if(arr[lastposs].x==i){
+				//this means we need to do something
+				if(!arr[lastposs].additionAction){
+					currentSegments.remove(arr[lastposs].pair);
 				}
-			}
-			if(arr[lastposs].xbeg==i){
-				for(LineSegment p: currentlyAdded){
-					if(interesect(p, arr[lastposs])){
-						oneposs=lastposs;
+				if(arr[lastposs].additionAction){
+					for(Point p: currentSegments){
+						if(interesect(arr[lastposs],p)){
+							intersections[arr[lastposs].i]++; intersections[p.i]++;
+							onepoint = arr[lastposs]; otherpoint= p;
+						}
 					}
+					currentSegments.add(arr[lastposs]);
 				}
-				currentlyAdded.add(arr[lastposs]);
-
+				lastposs++;
+			}
+			if(lastposs!= 2*n && arr[lastposs].x ==i){
+				i--;
 			}
 		}
-//		PrintWriter out = new PrintWriter(new File("cowjump.out"));
-//		out.println(pos+1);
-//		out.close();`
-	}
-	static class Point{
-		int x,y,i;
-		public Point(int xv, int yv){
-			x= xv; y =yv;
+		PrintWriter out = new PrintWriter(new File("cowjump.out"));
+		if(onepoint==null){
+			out.println(-1);
+		}else if(intersections[onepoint.i]>=intersections[otherpoint.i]){
+			out.println(onepoint.i+1);
+		}else{
+			out.println(otherpoint.i+1);
 		}
+		out.close();
+
 	}
-	static class LineSegment implements Comparable{
-		int xbeg; int ybeg;
-		int xend; int yend;
-		public LineSegment(int x0, int y0, int x1, int y1) {
-			xbeg = x0;
-			ybeg = y0;
-			xend = x1;
-			yend = y1;
+	static class Point implements Comparable{
+		int x,y,i;
+		Point pair; 
+		boolean additionAction;
+		public Point(int xv, int yv,int iv, boolean condition, Point otherpoint){
+			x= xv; y =yv; i =iv;  additionAction= condition; pair = otherpoint;
+			//might have pair equal to null but changed later -> sus programming
 		}
 
 		@Override
 		public int compareTo(Object o) {
-			LineSegment p = (LineSegment)o;
-			if(xbeg==p.xbeg){
-				return ybeg-p.ybeg;
+			Point p = (Point)o;
+			if(x!=p.x){
+				return x-p.x;
+			}else if(additionAction!=p.additionAction){
+				if(!additionAction){ //better to first remove the point (when adding have to check if intersection and gauran no intersect here (x and y /= for both)
+					return 1;
+				}else{
+					return -1;
+				}
 			}else{
-				return xbeg- p.xbeg;
+				return y- p.y;
 			}
 		}
 
 		@Override
 		public String toString() {
-			return "LineSegment{" +
-					"xbeg=" + xbeg +
-					", ybeg=" + ybeg +
-					", xend=" + xend +
-					", yend=" + yend +
+			return "Point{" +
+					"x=" + x +
+					", y=" + y +
+					", i=" + i +
+					", additionAction=" + additionAction +
 					'}';
 		}
 	}
-	public static boolean interesect(LineSegment line1, LineSegment line2){
-		int x0= line1.xbeg; int y0= line1.ybeg; int x1 = line1.xend; int y1 = line1.yend;
-		int x2= line2.xbeg; int y2= line2.ybeg; int x3 = line2.xend; int y3 = line2.yend;
+
+	public static boolean interesect(Point point1, Point point2){
+		int x0= point1.x; int y0= point1.y; int x1 = point1.pair.x; int y1 = point1.pair.y;
+		int x2= point2.x; int y2= point2.y; int x3 = point2.pair.x; int y3 = point2.pair.y;
 		double s1x = x1-x0; double s1y = y1 - y0;
 		double s2x = x3-x2; double s2y = y3 - y2;
 		double v = -s2x * s1y + s1x * s2y;
