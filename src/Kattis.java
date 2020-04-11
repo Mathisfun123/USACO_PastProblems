5eadacfb8a585545dc62d02aad939df400aed3a3
Mathisfun123 <sairajareddykurelli@gmail.com>
2020-04-11 14:48:33 -0500

    Working on kattis -> bit not working

--- src/Kattis.java
+++ src/Kattis.java
@@ -7,58 +7,41 @@ import java.util.*;
 public class Kattis {
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner sc = new Scanner(System.in);
-		while(sc.hasNextLine()) {
-			int n = sc.nextInt();
-			Stack<Integer> stack = new Stack<>(); boolean isStack = true;
-			Queue<Integer> queue = new LinkedList<>(); boolean isQueue = true;
-			PriorityQueue<CompareInts> priorityQueue = new PriorityQueue<>(); boolean isPriority = true;
-			for (int i = 0; i < n; i++) {
-				int command = sc.nextInt(); int val = sc.nextInt();
-				if(command==1){
-					stack.add(val);
-					queue.add(val) ;
-					priorityQueue.add(new CompareInts(val));
-				}else{
-					if(stack.size()==0 || val!=stack.pop()) {
-						isStack = false;
-					}
-					if(queue.size()==0 || val!= queue.remove()){
-						isQueue= false;
-					}
-					if(priorityQueue.size()==0|| val!= Objects.requireNonNull(priorityQueue.poll()).val){
-						isPriority=false;
+		int n= 50;
+		while(sc.hasNextLine() && n!=0) {
+			n = Integer.parseInt(sc.nextLine());
+			if(n!=0) {
+				Boolean[] bits = new Boolean[32];
+				for (int i = 0; i < n; i++) {
+					String ln[] = sc.nextLine().split(" ");
+					if (ln[0].equals("SET")) {
+						bits[Integer.parseInt(ln[1])] = Boolean.TRUE;
+					} else if (ln[0].equals("CLEAR")) {
+						bits[Integer.parseInt(ln[1])] = Boolean.FALSE;
+					} else if (bits[Integer.parseInt(ln[1])] != null && bits[Integer.parseInt(ln[2])] != null && ln[0].equals("OR")) {
+						bits[Integer.parseInt(ln[1])] = bits[Integer.parseInt(ln[1])] || bits[Integer.parseInt(ln[2])];
+					} else if (bits[Integer.parseInt(ln[1])] != null && ln[0].equals("AND")) {
+						if (bits[Integer.parseInt(ln[2])] != null) {
+							bits[Integer.parseInt(ln[1])] = bits[Integer.parseInt(ln[1])] && bits[Integer.parseInt(ln[2])];
+						} else {
+							bits[Integer.parseInt(ln[1])] = null;
+						}
 					}
 				}
+				StringBuilder s = new StringBuilder();
+				for (int i = 31; i >= 0; i--) {
+					if (bits[i] == null) {
+						s.append("?");
+					} else if (bits[i] == Boolean.FALSE) {
+						s.append("0");
+					} else {
+						s.append("1");
+					}
+				}
+				System.out.println(s.toString());
 			}
-			if(!isStack && !isQueue && !isPriority){
-				System.out.println("impossible");
-			}else if(!isStack && !isQueue){
-				System.out.println("priority queue");
-			}else if(!isQueue && !isPriority){
-				System.out.println("stack");
-			}else if(!isStack &&!isPriority){
-				System.out.println("queue");
-			}else{
-				System.out.println("not sure");
-			}
+
 		}
 
-	}
-	static class CompareInts implements Comparable{
-		int val;
-
-		public CompareInts(int i) {
-			val = i;
-		}
-
-		@Override
-		public String toString() {
-			return "" +val;
-		}
-
-		@Override
-		public int compareTo(Object o) {
-			return -val+((CompareInts)o).val;
-		}
 	}
 }
